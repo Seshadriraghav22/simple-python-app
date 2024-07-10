93377a2d3b69326ebe8bfb0d55b770d78c5736b2
Seshadri Raghav <165247517+Seshadriraghav22@users.noreply.github.com>
2024-07-10 16:07:10 +0530

    Create start_container.sh

---
+++ start_container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull seshadriraghav22/simple-python-flask-app:latest
+# Run the Docker image as a container
+docker run  -d -p 5000:5000 seshadriraghav22/simple-python-flask-app:latest
